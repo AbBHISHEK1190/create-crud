bb615446f5d3d841cb88d5be9aa0b0cac18af992
AbBHISHEK1190 <kumarjha271190@gmail.com>
2023-01-07 23:36:53 +0530

    p

--- src/Register.js
+++ src/Register.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { Await } from "react-router-dom";
 
 function Register() {
-  const [username, setUsername] = useState("");
+  const [name, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 // const history=useHistory();
   const signup=async ()=>{
 
-    let item = { username, email, password };
+    let item = { name, email, password };
     console.warn(item);
-    let result = await fetch("http://localhost/restapi/index.php/users/signup", {
+    let result = await fetch("http://127.0.0.1:8000/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,24 +22,13 @@ function Register() {
     console.warn("result",result);
     // history.push('/add');
   }
-  function signup_old() {
-    let item = { username, email, password };
-    console.warn(item);
-    let result = fetch('http://localhost/restapi/index.php/users/signup', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify(item)
-    });
-  }
+  
   return (
     <div className="col-sm-6 offset-sm-3">
       <h1>Register</h1>
       <input
         type="text"
-        value={username}
+        value={name}
         onChange={(e) => setUsername(e.target.value)}
         className="form-control"
       />
